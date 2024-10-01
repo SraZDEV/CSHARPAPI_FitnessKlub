90f7cd522cdaee49c9c73f32ea8f4c5a57c4aae9
Danijela Varga <thekalazic@gmail.com>
2024-10-01 18:12:12 +0200

    .

--- Models/Clan.cs
+++ Models/Clan.cs
@@ -5,14 +5,14 @@ namespace CSHARPAPI_FitnessKlub.Models;
 public class Clan: Entitet
 {
 
-    public string? Ime { get; set; }
-    public string? Prezime { get; set; }
-    public string? Email { get; set; }
+    public required string Ime { get; set; }
+    public required string Prezime { get; set; }
+    public required string Email { get; set; }
     [ForeignKey("grupa")]
     public required Grupa Grupa { get; set; }
     [Column("clan_od")]
     public DateTime? ClanOd { get; set; }
-    public bool? Verificiran { get; set; }
+    public bool Verificiran { get; set; }
 
 
     public ICollection<PrivatniTrener>? PrivatniTreneri { get; } = [];
