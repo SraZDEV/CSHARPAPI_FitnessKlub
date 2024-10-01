67b20cd28c2d389c7500490b109cd7dc2751d780
Danijela Varga <thekalazic@gmail.com>
2024-10-01 18:11:09 +0200

    .

--- Models/Grupa.cs
+++ Models/Grupa.cs
@@ -5,12 +5,12 @@ namespace CSHARPAPI_FitnessKlub.Models;
 public class Grupa: Entitet
 {
 
-    public string? Naziv { get; set; }
+    public required string Naziv { get; set; }
     [ForeignKey("privatni_trener")]
     public required PrivatniTrener PrivatniTrener { get; set; }
     [Column ("kolicina_clanova")]
-    public int? KolicinaClanova { get; set; }
-    public int? Cijena { get; set; }
+    public int KolicinaClanova { get; set; }
+    public int Cijena { get; set; }
 
     
 
